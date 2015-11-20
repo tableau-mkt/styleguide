fc8158f23a32c3912d3786e146a2637aa6e97a59
Travis Deployment <visualanalysis@tableau.com>
2015-11-20 18:08:10 +0000

    Auto-deploy via Travis CI

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -516,8 +516,102 @@ in CSS as well.
   } 
 }( jQuery ));
 ;
+/**
+ * Section search behaviors.
+ *
+ * - Toggle .is-open state on component, e.g., upon AJAX search result.
+ * - Handle down/up arrow keys on pick list
+ */
+
+// Loose augmentation pattern. Creates top-level Tabia variable if it doesn't
+// already exist.
+var Tabia = Tabia || {};
+
+// Create a base for this module's data and functions.
+Tabia.contextualSearch = {};
+
+/**
+ * DOM-ready callback.
+ *
+ * @param {Object} $
+ *   jQuery
+ */
+Tabia.contextualSearch.ready = function ($) {
+  // Set up all the section search components on the page.
+  $('.contextual-search').each(function () {
+    var $this = $(this),
+        // Initialze a data object for this instance.
+        search = {
+          selectionIndex: -1
+        };
+    // Save a reference to this element.
+    search.element = this;
+    // Attach keydown handler with our data context.
+    $this.keydown($.proxy(Tabia.contextualSearch.keydownHandler, search));
+    // Attach UI click handler. Don't propagate clicks to document.
+    $this.find('.contextual-search__ui').click(function (event) {
+      event.stopPropagation();
+    });
+    // Attach document click handler to close (blur) the results list.
+    $(document).click(function contextualSearchBlur() {
+      $(search.element).removeClass('is-open');
+    });
+    // Attach reset handler.
+    $this.find('.contextual-search__reset').click(function contextualSearchReset() {
+      $(search.element).removeClass('is-open');
+    });
+  });
+};
+
+/**
+ * Keydown handler.
+ *
+ * @param {Object} event
+ */
+Tabia.contextualSearch.keydownHandler = function (event) {
+  // Only handle keys when the results list is open.
+  if (!$(this.element).hasClass('is-open')) {
+    return;
+  }
+
+  switch (event.which) {
+    case 38: // UP
+      Tabia.contextualSearch.select.call(this, -1);
+      break;
+    case 40: // DOWN
+      Tabia.contextualSearch.select.call(this, 1);
+      break;
+    case 27: // ESCAPE
+      $(this.element).removeClass('is-open');
+      break;
+    case 13: // ENTER
+      Tabia.contextualSearch.select(0);
+      this.$rows.get(this.selectionIndex).click();
+      event.preventDefault();
+      break;
+  }
+};
+
+/**
+ * Set the row selection up/current/down.
+ *
+ * @param {Number} direction
+ *   -1, 0, or 1
+ */
+Tabia.contextualSearch.select = function (direction) {
+  this.$rows = $(this.element).find('.contextual-search__results-row');
+  this.selectionIndex += direction;
+  this.selectionIndex = Math.max(this.selectionIndex, 0);
+  this.selectionIndex = Math.min(this.selectionIndex, this.$rows.length - 1);
+  this.$rows.removeClass('is-selected')
+    .eq(this.selectionIndex).addClass('is-selected');
+};
+
+// Attach our DOM-ready callback.
+jQuery(Tabia.contextualSearch.ready);
+;
 $(document).ready(function () {
-  $('input, select, textarea').not('[type="checkbox"], [type="radio"]').closest('.form-field__wrapper').floatLabel();
+  $('.has-float-label').find('input, select, textarea').not('[type="checkbox"], [type="radio"]').closest('.form-field__wrapper').floatLabel();
 });
 ;
 /**
--- build/js/styleguide.js
+++ build/js/styleguide.js
@@ -1,3 +1,41 @@
+/**
+ * Section search styleguide integration.
+ */
+
+jQuery(function ready($) {
+  $('.contextual-search__input').keydown(function (event) {
+    var keyCode = $.ui.keyCode;
+
+    switch (event.keyCode) {
+      case keyCode.UP:
+      case keyCode.DOWN:
+      case keyCode.ESCAPE:
+      case keyCode.ENTER:
+        break;
+      default:
+        $(this).parents('.contextual-search').addClass('is-open');
+    }
+
+  });
+});
+;
+/**
+ * Video playlist script for demo purposes only.
+ */
+(function ($, window) {
+  $(document).ready(function () {
+    var $playlistVideo = $('.video-playlist__link');
+
+    $playlistVideo.click(function playlistVideoClick(e) {
+      e.preventDefault();
+
+      $('.video-playlist__item-wrapper.is-active').removeClass('is-active');
+      $(this).closest('.video-playlist__item-wrapper').addClass('is-active');
+    });
+
+  });
+})(jQuery, window);
+;
 // Fake the 'tabAjaxMegaMenu:ready' event in order to demonstrate the navigation
 // within the styleguide.
 $(document).ready(function() {
